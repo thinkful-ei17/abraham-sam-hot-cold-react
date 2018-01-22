7653c4b73aad07f1de967972c39a09c489c5332c
Abraham Cuenca <websiteprojectmanager@gmail.com>
2018-01-22 12:11:11 -0800

    Add what and new game links

--- src/game.js
+++ src/game.js
@@ -6,6 +6,8 @@ export default function Game(props){
   return(
     <div className="game">
       <h1>{props.title}</h1>
+      <a href="#">What</a>
+      <a href="#">+New Game</a> 
       <Feedback />
     </div>
   );
--- src/log.js
+++ src/log.js
@@ -2,15 +2,19 @@ import React from 'react';
 
 export default function Log(props){
     
-    const guesses = props.guesses.map((guess, index) => 
-        <li key={index}>
-            {guess}
-        </li>
-    );
+  const guesses = props.guesses.map((guess, index) => 
+    <li key={index}>
+      {guess}
+    </li>
+  );
 
-    return(
-        <ul>
-            {guesses}
-        </ul>
-    );
+  return(
+    <ul>
+      {guesses}
+    </ul>
+  );
 }
+
+Log.defaultProps = {
+  guesses: [],
+};
