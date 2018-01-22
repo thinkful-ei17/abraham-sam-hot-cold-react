3a18218e0aae7451786e24def6dd99e70441547d
Sam Gould <sjg326@gmail.com>
2018-01-22 13:33:50 -0800

    updated text for links

--- src/game.js
+++ src/game.js
@@ -7,8 +7,8 @@ export default function Game(props){
   return(
     <div className="game">
       <nav className="game-nav">
-        <a href="">What</a>
-        <a href="">+New Game</a> 
+        <a href="">WHAT</a>
+        <a href="">+NEW GAME</a> 
       </nav>
       <h1>{props.title}</h1>
       <Feedback />
