6b93eba7956f7cd29e18708c7538b4931c0069f3
Abraham Cuenca <websiteprojectmanager@gmail.com>
2018-01-22 11:36:24 -0800

    Add Game and Feedback components

---
+++ src/feedback.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Feedback(props){
+  // Logic for hot/cold?
+  return(
+    <div className="feedback">
+      <p>Hello World</p>
+    </div>
+  );
+  
+}
---
+++ src/game.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Feedback from './feedback';
+
+export default function Game(){
+  return(
+    <div className="game">
+    <Feedback />
+    </div>
+  );
+}
